0aa38fc06172106d0d8d030d0fdf63e6e7dc34e2
Dasun110 <dasunpcc50@gmail.com>
2024-04-18 19:55:21 +0530

    portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isProd =process.env.NODE_ENV ='production';
-
 const nextConfig = {
-  basePath:isProd? '/DasunSathsara':'',
-  output:'export',
   reactStrictMode: true,
-  distDir:'dist',
-  images:{
-    unoptimized:true,
-  }
-
 };
 
 export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+// Import global styles
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
